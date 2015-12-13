0464c0f93ee3cdebf76fec32ac81df720058c7c6
anbdesign <alex@anbdesign.com>
2015-12-12 16:37:44 -0800

    That's refreshing
    Added the levelCreate function
       currently demoed through red button
    
    Added the ability to reset the board with a button in the upper right
    
    next up, level selector.

--- Puzzle_for_Squares.framer/framer/framer.generated.js
+++ Puzzle_for_Squares.framer/framer/framer.generated.js
@@ -6,7 +6,7 @@ window.Framer.Defaults.DeviceView = {"deviceScale":-1,"deviceType":"iphone-6-spa
 
 window.Framer.Defaults.DeviceComponent = {"deviceScale":-1,"deviceType":"iphone-6-spacegray","contentScale":1,"orientation":0};
 
-window.FramerStudioInfo = {"deviceImagesUrl":"\/_server\/resources\/DeviceImages","documentTitle":"Ice Game v0.3.8.framer"};
+window.FramerStudioInfo = {"deviceImagesUrl":"\/_server\/resources\/DeviceImages","documentTitle":"Puzzle_for_Squares.framer"};
 
 Framer.Device = new Framer.DeviceView();
 Framer.Device.setupContext();
